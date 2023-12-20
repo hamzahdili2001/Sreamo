6c89d34a1fe65179307c9e7f4c06e1b2e30a0014
hamzahdili2001 <hdilihamza955@gmail.com>
2023-12-20 18:08:26 +0100

    [add] Creating database table

--- streamo_app/models.py
+++ streamo_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=500)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    videofile = models.FileField(upload_to='videos/', null=True, verbose_name="")
+
+    def __str__(self):
+        return self.name + ": " + str(self.videofile)
